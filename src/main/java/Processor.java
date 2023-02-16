66dfc1ba01e29324f7e06fd09223ae9da3a7d9af
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-02-16 23:22:42 +0530

    Add small comment

--- src/main/java/Processor.java
+++ src/main/java/Processor.java
@@ -1,3 +1,6 @@
+/**
+ * Processor should keep as an interface to use as a common parent for the all kind of Processors
+ */
 public interface Processor {
      void running();
 }
--- src/main/java/Ram.java
+++ src/main/java/Ram.java
@@ -1,3 +1,6 @@
+/**
+ * Ram should keep as an interface to use as a common parent for the all kind of Rams
+ */
 public interface Ram {
     void running();
 }

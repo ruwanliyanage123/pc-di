72c7b4d94b46d2fd7b40383262f38e0c0f0978ec
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-02-18 00:17:12 +0530

    Minor modificaion

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -3,7 +3,6 @@ package config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import processor.IntelProcessor;
 import processor.Processor;
 import ram.DDR3Ram;
@@ -13,11 +12,10 @@ import ram.Ram;
 @ComponentScan(basePackages = { "processor", "ram" })
 public class AppConfig {
 
-@Bean
-@Scope("prototype")
-public Processor intelProcessor() {
-    return new IntelProcessor();
-}
+    @Bean
+    public Processor intelProcessor() {
+        return new IntelProcessor();
+    }
 
     @Bean
     public Ram ddr3Ram() {

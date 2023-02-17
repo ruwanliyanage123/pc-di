28132bce8ba687110905377a750df0755d329722
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-02-17 19:02:57 +0530

    Add annotations for the dependency variables with the name annotations

--- src/main/java/Laptop.java
+++ src/main/java/Laptop.java
@@ -1,17 +1,13 @@
-import com.google.inject.Inject;
-
+import javax.inject.Inject;
 import javax.inject.Named;
 
 public class Laptop {
+    @Inject
+    @Named("amd")
     private Processor intelProcessor;
 
-    private Ram ddr3Ram;
-
     @Inject
-    public Laptop(@Named("amd") Processor processor, Ram ddr3Ram){
-        this.intelProcessor = processor;
-        this.ddr3Ram = ddr3Ram;
-    }
+    private Ram ddr3Ram;
 
     public void start(){
         System.out.println("Laptop properties: processor-"+intelProcessor +"and ram-"+ddr3Ram);

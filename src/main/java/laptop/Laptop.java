5b803c50f5ad17c090f27a27e09b16ee826ef1a6
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-02-19 09:09:08 +0530

    Removed the constructor and keep only with the Autowired annotations

--- src/main/java/laptop/Laptop.java
+++ src/main/java/laptop/Laptop.java
@@ -1,18 +1,16 @@
 package laptop;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import processor.Processor;
 import ram.Ram;
 
 @Component
 public class Laptop {
+    @Autowired
     private Processor intelProcessor;
+    @Autowired
     private Ram ddr3Ram;
-
-    public Laptop(Processor intelProcessor, Ram ddr3Ram) {
-        this.intelProcessor = intelProcessor;
-        this.ddr3Ram = ddr3Ram;
-    }
 
     public void start(){
         System.out.println("Laptop properties: processor-"+intelProcessor +"and ram-"+ddr3Ram);

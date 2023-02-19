df60b28a6b8497e51a65ba5fef4e7e971df40f50
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-02-19 10:52:26 +0530

    Add customer annotations into the constructor

--- src/main/java/laptop/Laptop.java
+++ src/main/java/laptop/Laptop.java
@@ -1,6 +1,5 @@
 package laptop;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import processor.Processor;
 import ram.Ram;
@@ -8,12 +7,13 @@ import ram.Ram;
 @Component
 public class Laptop {
 
-    @Autowired
-    @IntelProcessor
-    private Processor intelProcessor;
-    @Autowired
-    @DDR3Ram
-    private Ram ddr3Ram;
+    private final Processor intelProcessor;
+    private final Ram ddr3Ram;
+
+    public Laptop(@AMDProcessor Processor intelProcessor, @DDR4Ram Ram ddr3Ram) {
+        this.intelProcessor = intelProcessor;
+        this.ddr3Ram = ddr3Ram;
+    }
 
     public void start(){
         System.out.println("Laptop properties: processor-"+intelProcessor +"and ram-"+ddr3Ram);

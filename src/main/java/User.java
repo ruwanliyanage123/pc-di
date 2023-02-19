ae098b787dadf750d106160d3078fb03a2115fad
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-02-19 11:48:53 +0530

    Add spring xml configuration

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,5 +1,11 @@
+import laptop.Laptop;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class User {
     public static void main(String[] args) {
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        Laptop laptop = context.getBean(Laptop.class);
+        laptop.start();
     }
 }

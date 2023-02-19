06891b2efd1c937c3c99835fb0f3697e13b73036
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-02-19 11:34:20 +0530

    Reset the project to migrate with the spring xml configurations

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,10 +1,5 @@
-import laptop.Laptop;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 public class User {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        Laptop laptop = context.getBean(Laptop.class);
-        laptop.start();
+
     }
 }
--- src/main/java/laptop/Laptop.java
+++ src/main/java/laptop/Laptop.java
@@ -1,16 +1,14 @@
 package laptop;
 
-import org.springframework.stereotype.Component;
 import processor.Processor;
 import ram.Ram;
 
-@Component
 public class Laptop {
 
     private final Processor intelProcessor;
     private final Ram ddr3Ram;
 
-    public Laptop(@AMDProcessor Processor intelProcessor, @DDR4Ram Ram ddr3Ram) {
+    public Laptop(Processor intelProcessor, Ram ddr3Ram) {
         this.intelProcessor = intelProcessor;
         this.ddr3Ram = ddr3Ram;
     }
